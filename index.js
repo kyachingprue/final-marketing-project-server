3337835d6ae737d8b50269697c69b60dc5507fd5
kyachingprue <kyachingpruemarma.info@gmail.com>
2025-08-06 00:21:41 +0600

    final marketing project commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
@@ -50,6 +50,12 @@ async function run() {
     app.post('/carts', async (req, res) => {
       const cart = req.body;
       const result = await cartCollection.insertOne(cart);
+      res.send(result);
+    });
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
   } finally {
